9b1fef93446c09eba546a4cfe584171cb7ab199c
Roei Rom <roeirom6@gmail.com>
2022-05-21 21:30:09 +0300

    feat(index.ts): add middleware

--- index.ts
+++ index.ts
@@ -6,7 +6,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/users', usersRouter)
+app.use((req, res, next) => {
+    console.log('user launched ' + req.method + ' request to: ' + req.path + " at: " + (new Date()).toLocaleString());
+    next();
+});
+
+app.use('/users', usersRouter);
 
 app.listen(3000, () => {
     console.log('app listening on port 3000')
