49737bcbdd3d0fdce0a29441c5f870142b8e0a53
Roei Rom <roeirom6@gmail.com>
2022-05-21 21:23:05 +0300

    feat(): initial users route

--- index.ts
+++ index.ts
@@ -2,12 +2,27 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send('hello world!')
+const users = [
+    {
+        id: 1,
+        name: 'Roei Rom'
+    }
+];
+
+app.use(express.json());
+
+app.get('/users', (req, res, next) => {
+    res.send(users);    
 });
 
-app.post('/', (req, res, next) => {
-    res.send('hello world post!')
+app.get('/users/:userId', (req, res, next) => {
+    const { userId } = req.params;
+    res.send(users.find(user => user.id === +userId));
+});
+
+app.post('/users', (req, res, next) => {
+    users.push(req.body);
+    res.send('Added user!');
 });
 
 app.listen(3000, () => {
